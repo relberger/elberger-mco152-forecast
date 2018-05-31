15ebcf4bea57ee0d0d775ef2a248c5915f25d94a
Rachel Elberger <relberge@student.touro.edu>
2018-05-31 14:22:18 -0400

    single network call (untested)

--- src/elberger/forecast/ForecastController.java
+++ src/elberger/forecast/ForecastController.java
@@ -66,45 +66,48 @@ public class ForecastController
 	
 	void showForecast(int ii, ForecastFeedModel feed, JLabel time, JLabel icon, JLabel desc, JLabel high, JLabel low) throws MalformedURLException
 	{
-		List<Forecast> info = feed.getList();
-		
-		String timeStamp = info.get(ii).getDt_txt();
-		String timeStampSplit[] = timeStamp.split(" " , 2);
-		String fullTime = timeStampSplit[1];
-		String[] timeSplit = fullTime.split(":", 3); 
-		int hour = Integer.parseInt(timeSplit[0]);
-		if (hour == 12)
+		for(int ix = 0; ix < 8; ix++)
 		{
-			time.setText("12" + ":" + timeSplit[1] + " PM");						
-		}
-		else if (hour == 0)
-		{
-			time.setText("12" + ":" + timeSplit[1] + " AM");			
-		}
-		else if (hour < 12)
-		{
-			String hr = timeSplit[0];
-			String[] hourSplit = hr.split("0", 2);
-			time.setText(hourSplit[1] + ":" + timeSplit[1] + " AM");
-		}
-		else if (hour > 12)
-		{
-			int hr = Integer.parseInt(timeSplit[0]) - 12;
-			timeSplit[0] = String.valueOf(hr);
-			time.setText(timeSplit[0] + ":" + timeSplit[1] + " PM");
-		}
+			List<Forecast> info = feed.getList();
+			
+			String timeStamp = info.get(ii).getDt_txt();
+			String timeStampSplit[] = timeStamp.split(" " , 2);
+			String fullTime = timeStampSplit[1];
+			String[] timeSplit = fullTime.split(":", 3); 
+			int hour = Integer.parseInt(timeSplit[0]);
+			if (hour == 12)
+			{
+				time.setText("12" + ":" + timeSplit[1] + " PM");						
+			}
+			else if (hour == 0)
+			{
+				time.setText("12" + ":" + timeSplit[1] + " AM");			
+			}
+			else if (hour < 12)
+			{
+				String hr = timeSplit[0];
+				String[] hourSplit = hr.split("0", 2);
+				time.setText(hourSplit[1] + ":" + timeSplit[1] + " AM");
+			}
+			else if (hour > 12)
+			{
+				int hr = Integer.parseInt(timeSplit[0]) - 12;
+				timeSplit[0] = String.valueOf(hr);
+				time.setText(timeSplit[0] + ":" + timeSplit[1] + " PM");
+			}
 
-		ForecastWeather weather[] = info.get(ii).getWeather();
-		
-		URL iconUrl = new URL("http://openweathermap.org/img/w/" + weather[0].getIcon() + ".png");
-		ImageIcon image = new ImageIcon(iconUrl);
-		icon.setIcon(image);
-		
-		desc.setText(weather[0].getDescription());
-		
-		high.setText("High: " + String.valueOf(info.get(ii).getMain().getTemp_max()) + "°F");
-		
-		low.setText("Low: " + String.valueOf(info.get(ii).getMain().getTemp_min()) + "°F");
+			ForecastWeather weather[] = info.get(ii).getWeather();
+			
+			URL iconUrl = new URL("http://openweathermap.org/img/w/" + weather[0].getIcon() + ".png");
+			ImageIcon image = new ImageIcon(iconUrl);
+			icon.setIcon(image);
+			
+			desc.setText(weather[0].getDescription());
+			
+			high.setText("High: " + String.valueOf(info.get(ii).getMain().getTemp_max()) + "°F");
+			
+			low.setText("Low: " + String.valueOf(info.get(ii).getMain().getTemp_min()) + "°F");
+		}
 	}
 	
 	public void zipValidation() throws IOException
